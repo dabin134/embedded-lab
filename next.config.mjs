a575b9ce7794e3a5b3e969a9d342e09a137aaf72
dabin134 <kempkensdabin@gmail.com>
2026-09-12 21:18:59 +0200

    Configure static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isPages = process.env.GITHUB_PAGES === "true";
+const basePath = isPages ? "/embedded-lab" : "";
+
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
+  basePath,
+  assetPrefix: basePath,
 };
 
 export default nextConfig;
